76a283c80e0f95b38b29bfd99b48b0451c1194b7
a378gupt <a378gupt@uwaterloo.ca>
2024-02-20 23:34:40 -0500

    optimized imports

--- app/app/src/main/java/com/example/clubwat/views/MainActivity.kt
+++ app/app/src/main/java/com/example/clubwat/views/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
